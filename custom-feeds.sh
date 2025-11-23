52dacef7213088671e612c667bb68044cb40f105
Sergey Shlukov <ichizakurain@gmail.com>
2025-11-23 15:09:47 +0300

    Update custom-feeds.sh

--- custom-feeds.sh
+++ custom-feeds.sh
@@ -1,5 +1,5 @@
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' $BUILD_ROOT/feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' $BUILD_ROOT/feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >> $BUILD_ROOT/feeds.conf.default
